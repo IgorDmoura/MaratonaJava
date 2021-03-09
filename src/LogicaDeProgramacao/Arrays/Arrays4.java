65a6ea7e434cafa6181982ce01c42eef7c4db89f
IgorDmoura <igor.dmoura01@gmail.com>
2021-03-09 13:49:11 -0300

    Commit

--- src/LogicaDeProgramacao/Arrays/Arrays4.java
+++ src/LogicaDeProgramacao/Arrays/Arrays4.java
@@ -2,7 +2,7 @@ package LogicaDeProgramacao.Arrays;
 
 public class Arrays4 {
     public static void main(String[] args) {
-        String[] nomes = {"João" , "Pedro" , "Maria"};
+        String[] nomes = {"João" , "Pedro" , "Carlos"};
         for(String nome : nomes) {
             System.out.println(nome);
         }
